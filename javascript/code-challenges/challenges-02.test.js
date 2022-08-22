d1d6dda3404419c39ce3aff3687741ee7533d4fc
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-23 01:16:12 +0300

    almost done

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -58,7 +58,16 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 ------------------------------------------------------------------------------------------------ */
 
 const forLoopTwoToThe = (arr) => {
-  // Solution code here...
+  let arr2 = [];
+  for(let i = 0; i <arr.length; i++) {
+    let a = arr[i];
+    let b = 1;
+    for(let j= a; j > 0; j--){
+      b*=2;
+    }
+    arr2[i] =b;
+  }
+  return arr2;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -68,7 +77,14 @@ Write a function named forEachTwoToThe that produces the same output as your for
 ------------------------------------------------------------------------------------------------ */
 
 const forEachTwoToThe = (arr) => {
-  // Solution code here...
+  let arr2 = [];
+  arr.forEach((val, i) => {
+    let b = val;
+    let c = Math.pow(2, b);
+    arr2[i] =c;
+  }
+  );
+  return arr2;
 };
 
 /* ------------------------------------------------------------------------------------------------
